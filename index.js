568bcb6b6bfc80f4eea90c0ca3ad1a7d7c00fa93
saloni2805 <salonispotdar@gmail.com>
2025-05-17 23:16:15 +0530

    `Added database connection and CRUD operations for candidate data`

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const path = require("path")
 const app = express()
+var connection = require("./config/db")
+// var connection = require("./config/db")
 
 const PORT = process.env.PORT || 3000
 const HOST = process.env.HOST || "127.0.0.1"
@@ -55,14 +57,154 @@ app.get("/interview_calls/interview_details", (req, res) => {
 })
 
 // ------------> Recruitment <------------
-app.get("/recruitment", (req, res) => {
+app.get("/recruitment", async (req, res) => {
   res.render("recruitment/recruitment_index")
 })
 
 // -----------> job posts <------------
-app.get("/job_posts", (req, res) =>
-  res.render("recruitment/job_posts/job_posts_index")
-)
+app.get("/job_posts", async (req, res) => {
+  try {
+    var sql = `select * from candidate_data`
+    // result in the form of array object
+    const [result] = await connection.execute(sql)
+    const obj = { data: result }
+    console.log(obj)
+    res.render("recruitment/job_posts/job_posts_index", obj)
+    console.log("Data Fetched Successfully..")
+  } catch (err) {
+    console.log(err)
+    console.log("Data faild to fetch..")
+    return
+  }
+})
+
+app.get("/add_candidate", (req, res) => {
+  res.render("recruitment/job_posts/add_candidate")
+})
+
+app.post("/save_candidate_form", async (req, res) => {
+  try {
+    const {
+      candidate_name,
+      candidate_email,
+      candidate_gender,
+      candidate_experience,
+      job_post_type,
+    } = req.body
+    var sql = `insert into candidate_data (candidate_name,candidate_email,candidate_gender,candidate_experience,job_post_type,created_at) values('${candidate_name}','${candidate_email}','${candidate_gender}','${candidate_experience}','${job_post_type}',NOW())`
+    await connection.execute(sql)
+    console.log("Data Inserted Successfully....")
+    res.send(`<script>
+      alert('Candidate added successfully');
+      window.location.href='/job_posts'
+      </script>`)
+  } catch (err) {
+    console.log(err)
+    console.log("Faild to Insert Data....")
+    return
+  }
+})
+
+app.get("/delete_candidate/:id", async (req, res) => {
+  try {
+    var id = req.params.id
+    var sql = `delete from candidate_data where candidate_id='${id}'`
+    await connection.execute(sql)
+    res.send(`<script>
+      alert('Candidate deleted successfully');
+      window.location.href='/job_posts'
+      </script>`)
+  } catch (err) {
+    console.log(err)
+    console.log("Faild to Delete Candidate")
+  }
+})
+
+app.get("/view_candidate/:id", async (req, res) => {
+  try {
+    var id = req.params.id
+    var sql = `select * from candidate_data where candidate_id='${id}'`
+    const [result] = await connection.execute(sql)
+    const obj = { data: result[0] }
+    console.log(obj)
+    res.render("recruitment/job_posts/view_candidate", obj)
+    // res.send(`<script>
+    //   alert('Interview scheduled successfully');
+    //   window.location.href='/job_posts'
+    //   </script>`)
+  } catch (err) {
+    console.log(err)
+    console.log("Faild to view Candidate")
+  }
+})
+
+app.post("/schedule_interview", async (req, res) => {
+  try {
+    const {
+      candidate_name,
+      candidate_email,
+      candidate_gender,
+      candidate_experience,
+      job_post_type,
+      scheduled_on,
+      remark,
+    } = req.body
+    var sql = `insert into schedule_interview (candidate_name,candidate_email,candidate_gender,candidate_experience,job_post_type, scheduled_on, remark, created_at) values('${candidate_name}','${candidate_email}','${candidate_gender}','${candidate_experience}','${job_post_type}','${scheduled_on}','${remark}',NOW())`
+    await connection.execute(sql)
+    console.log("Data Inserted Successfully....")
+    res.send(`<script>
+      alert('Interview scheduled successfully');
+      window.location.href='/job_posts'
+      </script>`)
+  } catch (err) {
+    console.log(err)
+    console.log("Faild to Insert Data....")
+    return
+  }
+})
+
+app.get("/edit_candidate/:id", async (req, res) => {
+  try {
+    var id = req.params.id
+    var sql = `select * from candidate_data where candidate_id='${id}'`
+    const [result] = await connection.execute(sql)
+    const obj = { data: result[0] }
+    console.log(obj)
+    res.render("recruitment/job_posts/edit_candidate", obj)
+  } catch (err) {
+    console.log(err)
+    console.log("Data faild to fetched ....Edit User")
+  }
+})
+
+app.post("/update_candidate_form", async (req, res) => {
+  try {
+    const {
+      candidate_name,
+      candidate_email,
+      candidate_gender,
+      candidate_experience,
+      job_post_type,
+      id,
+    } = req.body
+
+    var sql = `update candidate_data
+        set 
+       candidate_name='${candidate_name}',
+        candidate_email='${candidate_email}',
+        candidate_gender='${candidate_gender}',
+        candidate_experience='${candidate_experience}',
+        job_post_type='${job_post_type}'
+        where candidate_id='${id}'`
+
+    await connection.execute(sql)
+
+    res.redirect("/job_posts")
+  } catch (err) {
+    console.log(err)
+    console.log("Faild to update data")
+  }
+})
 
 // ----------> schedule interview <----------
 app.get("/scheduled_interview", (req, res) =>
